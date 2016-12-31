d3324a11723daab78316b005cbf9858d81a6135e
Muzammil Momin <muzammilmomin@live.com>
2016-12-31 23:26:23 +0300

    Added ?completed

--- server.js
+++ server.js
@@ -16,12 +16,27 @@ app.get('/', function(req, res) {
 //GET /todos
 
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	console.log(req.params.id);
+	//debugger;
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	//if has property and completed is equal to true
+	// filteredTodos = _.where(filteredTodos,)
+	// else if has prop and completed if 'false'
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed : false});
+	}
+	res.json(filteredTodos);
 })
 
 //GET /todos/:id
 
 app.get('/todos/:id', function(req, res) {
+	console.log(req.params.id);
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {
 		id: todoId
