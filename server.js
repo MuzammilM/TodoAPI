54412e7836696a96a45479f0e7b1bff9a93569c7
Muzammil Momin <muzammilmomin@live.com>
2017-01-01 16:55:25 +0300

    Final static todos

--- server.js
+++ server.js
@@ -13,22 +13,24 @@ app.get('/', function(req, res) {
 	res.send('To do API');
 })
 
-//GET /todos
+//GET /todos?completed=true&q=work
 
 app.get('/todos', function(req, res) {
-	console.log(req.params.id);
-	//debugger;
+
 	var queryParams = req.query;
 	var filteredTodos = todos;
-
-	//if has property and completed is equal to true
-	// filteredTodos = _.where(filteredTodos,)
-	// else if has prop and completed if 'false'
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos = _.where(filteredTodos, {completed : false});
+	}
+
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos, function (todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q) > -1 ;
+		});
 	}
 	res.json(filteredTodos);
 })
