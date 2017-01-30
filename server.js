4f8202ff683219abc19877f1afb12796986eeab4
MuzammilM <momin.muzammil@machinepulse.com>
2017-01-30 11:57:25 +0000

    Add login route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-//404 - Not Found ; 400 Incorrect Syntax
+//404 - Not Found ; 400 Incorrect Syntax ; 401 authentication is possible but failed
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-var db = require('./db.js')
+var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -128,13 +129,26 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user){
-		res.json(user.toPublicJSON())
+		res.json(user.toPublicJSON());
 	},function(e){
-		res.status(400).json(e);
+		res.status(401).json(e);
 	});
 });
 
-db.sequelize.sync().then(function(){
+//POST /users/login
+
+app.post('/users/login', function(req,res){
+	var body = _.pick(req.body,'email','password');
+
+	db.user.authenticate(body).then(function (user){
+		res.json(user.toPublicJSON());
+	}, function (){
+		res.status(401).send();
+	});
+
+});
+
+db.sequelize.sync({force: true}).then(function(){
 	app.listen(PORT, function() {
 	console.log('Express listening on ' + PORT + ' !');
 });	
