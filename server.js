9b95b1f895cdb0e742dfbf91ff0acc7f16ee5ea4
MuzammilM <momin.muzammil@machinepulse.com>
2017-02-01 09:26:53 +0000

    Add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,7 +19,7 @@ app.get('/', function(req, res) {
 
 //GET /todos?completed=true&q=work
 
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 
 	var query = req.query;
 	var where = {};
@@ -44,7 +45,7 @@ app.get('/todos', function(req, res) {
 
 //GET /todos/:id
 
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	console.log(req.params.id);
 	var todoId = parseInt(req.params.id, 10);
 	db.todo.findById(todoId).then(function(todo){
@@ -61,7 +62,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 
 	var body = _.pick(req.body, 'description', 'completed');
 
@@ -72,7 +73,7 @@ app.post('/todos', function(req, res) {
 	});
 });
 
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	
 	db.todo.destroy({
@@ -96,7 +97,7 @@ app.delete('/todos/:id', function(req, res) {
 
 //PUT /todos/:id
 
-app.put('/todos/:id',function (req,	res){
+app.put('/todos/:id', middleware.requireAuthentication,function (req,	res){
 	var body = _.pick(req.body, 'description', 'completed');
 	var todoId = parseInt(req.params.id, 10);
 	var attributes = {};
