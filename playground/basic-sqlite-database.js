64dd8aed03b2f0fecce47e140593ed749d913a04
MuzammilM <momin.muzammil@machinepulse.com>
2017-02-09 11:25:50 +0000

    Adding todo/user assosciation

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,65 +1,60 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined,	{
-	'dialect': 'sqlite',
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+  'dialect': 'sqlite',
 
-	'storage': __dirname+'/basic-sqlite-database.sqlite'
+  'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
 
-var Todo = sequelize.define('todo',{
-	description: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		validate: {
-			len: [1,250]
-		}
+var Todo = sequelize.define('todo', {
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 250]
+    }
 
-	},
-	completed: {
-		type: Sequelize.BOOLEAN,
-		allowNull: false,
-		defaultValue: false
-	}
-})
+  },
+  completed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
+});
 
-sequelize.sync(/*{force: true}*/).then(function(){
-	console.log('Everything is synced');
+var User = sequelize.define('user',{
+	email : Sequelize.STRING
+});
 
-	
 
-Todo.findById(2).then(function (todo){
-	if (todo){
-		console.log(todo.toJSON());
-	} else {
-		console.log('Not Found');
-	}
-})
-	/*Todo.create({
-		description: 'Take out trash'
-	}).then(function(todo){
-		return Todo.create({
-			description: 'Clean office'
-		});
-	}).then(function (){
-		//return Todo.findById(1)
-		return Todo.findAll({
-			where:{
-				description: {
-					$like: '%Office%'
-				}
-			}
-		})
-	}).then(function (todos){
-		if(todos){
-			todos.forEach(function (todo){
-			console.log(todo.toJSON());	
-			})
-			
-		} else {
-			console.log('no todo found');
-		}
-	}).catch(function(e){
-		console.log(e);
-	});
-	*/
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+  //force: true
+}).then(function() {
+  console.log('Everything is synced');
+  User.findById(1).then(function (user){
+  	user.getTodos({
+  		where: {
+  			completed: false
+  		}
+  	}).then(function (todos){
+  		todos.forEach(function (todo){
+  			console.log(todo.toJSON());
+  		});
+  	});
+
+  })
+ /* User.create({
+  	email: 'mzammil@machinepulse.com'
+  }).then(function (){
+  	return Todo.create({
+  		description: 'Clean yard'
+  	});
+  }).then(function (todo){
+  	User.findById(1).then(function(user){
+  		user.addTodo(todo);
+  	});
+  });*/
 });
 
